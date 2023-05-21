ff931e5c26630d2e7d1683b60fb58be76171711d
Heavenston <malolegendrelemaire@@gmail.com>
2023-05-21 14:40:03 +0100

    Reorganise

---
+++ src/app.rs
@@ -0,0 +1,38 @@
+
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
+pub fn start_app() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                control_flow.set_exit();
+            },
+            Event::MainEventsCleared => {
+
+
+                window.request_redraw();
+            },
+            Event::RedrawRequested(_) => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in MainEventsCleared, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+            },
+            _ => ()
+        }
+    });
+}
